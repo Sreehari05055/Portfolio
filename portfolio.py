721ed71a1689aa81d669872d214edd3456d87849
SREEPC\sreek <sreekuttankzm@gmail.com>
2024-10-06 12:01:39 +0100

    Adding Tasks

--- portfolio.py
+++ portfolio.py
@@ -24,6 +24,6 @@ def emp_display():
 @app.route('/admin-functions')
 def admin_func():
     return render_template('admin-functions.html')
-
+#main
 if __name__ == '__main__':
     app.run(debug=True)
