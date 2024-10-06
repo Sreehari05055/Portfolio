db0c61347a057c7db8c908667be50a80e2509ba2
SREEPC\sreek <sreekuttankzm@gmail.com>
2024-10-06 10:08:00 +0100

    Adding Tasks

--- portfolio.py
+++ portfolio.py
@@ -16,13 +16,6 @@ def employee_management():
 @app.route('/turtle_graphics')
 def turtle_graphics():
     return render_template('turtle_graphics.html')
-
-
-@app.route('/chatbot_system')
-def chatbot_sys():
-    return render_template('chatbot_system.html')
-
-
 @app.route('/empManPic')
 def emp_display():
     return render_template('empManPic.html')
@@ -31,7 +24,6 @@ def emp_display():
 @app.route('/admin-functions')
 def admin_func():
     return render_template('admin-functions.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
