4289de6693125ae7e02817860e672b9f553355f5
SREEPC\sreek <sreekuttankzm@gmail.com>
2024-12-17 21:59:43 +0000

    Adding Portfolio

--- portfolio.py
+++ portfolio.py
@@ -17,13 +17,20 @@ def employee_management():
 def turtle_graphics():
     return render_template('turtle_graphics.html')
 
+
 @app.route('/empManPic')
 def emp_display():
     return render_template('empManPic.html')
 
+
 @app.route('/admin-functions')
 def admin_func():
     return render_template('admin-functions.html')
+
+
+@app.route('/sa-tool')
+def sa_func():
+    return render_template('sa-application.html')
 
 
 if __name__ == '__main__':
